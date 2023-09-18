cf0ed442c0b07840090a36eaaca353ae4b310551
Micah Eckard <lumberjacksoul339@gmail.com>
2023-09-18 19:58:46 -0400

    fixed some css in clock and added dome

--- app/page.js
+++ app/page.js
@@ -9,12 +9,20 @@ import { Suspense } from 'react'
 export default function Home() {
   return (
     <div className={styles.home}>
+        <div className={styles.dome}></div>
         <div className={styles.tower}>
           <div className={styles.clockback}>
-            <div className={styles.clock}></div>
+            <div className={styles.clock}>
+              <div className={styles.clockFace2}>---&#62;</div>
+              <div className={styles.clockFace1}>---&#62;</div>
+            </div>
           </div>
         </div>
-        <div className={styles.roof}></div>
+        <div className={styles.roof}>
+          <div className={styles.window1}></div>
+          <div className={styles.window2}></div>
+          <div className={styles.window3}></div>
+        </div>
         <div className={styles.building}></div>
         <Suspense fallback={<Loading/>}>
           <HomeWeather/>
